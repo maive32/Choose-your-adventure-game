a1ebc97e7c896b06f6b601dce5fef92041ac3f76
maive32 <94019913+maive32@users.noreply.github.com>
2022-04-06 15:02:07 -0700

    d

--- main.js
+++ main.js
@@ -10,21 +10,34 @@ const story = { //story object
             ],
             imgSrc: "image.jpg"
         },
-        Choice: {
+        Choice1: {
             text: `<h2>C1A</h2> Nibh nisl condimentum id venenatis a condimentum vitae. Et ligula ullamcorper malesuada proin libero. Ut aliquam purus sit amet luctus venenatis lectus magna fringilla. Tellus pellentesque eu tincidunt tortor aliquam. Morbi leo urna molestie at elementum. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Fermentum leo vel orci porta non pulvinar neque laoreet. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Non quam lacus suspendisse faucibus interdum posuere lorem ipsum. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Lectus sit amet est placerat in egestas erat imperdiet sed. Diam vulputate ut pharetra sit amet aliquam id diam.`,
             choices: [
-                ['Go to C1C','name'],
-                ['Go To C2','name']
+                ['Choice1','name'],
+                ['Choice3','name']
             ]
         },
-        Choice: {
+        Choice2: {
             text: `<h2>C1B</h2> Leo vel fringilla est ullamcorper eget nulla. Auctor eu augue ut lectus arcu bibendum at varius. Ipsum faucibus vitae aliquet nec. Semper feugiat nibh sed pulvinar. Proin nibh nisl condimentum id venenatis. Mattis aliquam faucibus purus in massa. Ornare suspendisse sed nisi lacus sed viverra tellus. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Amet mauris commodo quis imperdiet massa. Curabitur vitae nunc sed velit dignissim sodales ut. Ac turpis egestas maecenas pharetra. Volutpat est velit egestas dui id. Facilisis sed odio morbi quis commodo odio.`,
             choices: [
-                ['Go to C1A','name'],
-                ['Go To C2','name']
+                ['Choice3','name'],
+                ['Choice4','name']
             ]
         },
+        Choice3: {
+            text: `<h2>C1A</h2> Nibh nisl condimentum id venenatis a condimentum vitae. Et ligula ullamcorper malesuada proin libero. Ut aliquam purus sit amet luctus venenatis lectus magna fringilla. Tellus pellentesque eu tincidunt tortor aliquam. Morbi leo urna molestie at elementum. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Fermentum leo vel orci porta non pulvinar neque laoreet. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Non quam lacus suspendisse faucibus interdum posuere lorem ipsum. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Lectus sit amet est placerat in egestas erat imperdiet sed. Diam vulputate ut pharetra sit amet aliquam id diam.`,
+            choices: [
+                ['Choice4','name'],
+            ]
+        },
+        Choice4: {
+            text: `<h2>C1A</h2> Nibh nisl condimentum id venenatis a condimentum vitae. Et ligula ullamcorper malesuada proin libero. Ut aliquam purus sit amet luctus venenatis lectus magna fringilla. Tellus pellentesque eu tincidunt tortor aliquam. Morbi leo urna molestie at elementum. Ornare aenean euismod elementum nisi quis eleifend quam adipiscing vitae. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Vel elit scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Fermentum leo vel orci porta non pulvinar neque laoreet. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim. Non quam lacus suspendisse faucibus interdum posuere lorem ipsum. Cras semper auctor neque vitae tempus quam pellentesque nec nam. Lectus sit amet est placerat in egestas erat imperdiet sed. Diam vulputate ut pharetra sit amet aliquam id diam.`,
+            choices: []
+        }
 }
+
+var storyChoices = [];
+
 
 
 
